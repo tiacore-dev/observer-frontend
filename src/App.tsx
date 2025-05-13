ea66e922387f23676d0d0afb5dec242fccab2639
Marina <cherdantseva.mar@gmail.com>
2025-05-13 10:48:48 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from './context/LoginForm';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./context/LoginForm";
+//test push
 const HomePage: React.FC = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '48px',
-        fontWeight: 'bold',
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "48px",
+        fontWeight: "bold",
       }}
     >
       Добро пожаловать на главную страницу!
     </div>
-  );  
+  );
 };
 
 const App: React.FC = () => {
